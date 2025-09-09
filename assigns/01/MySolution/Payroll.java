5c0399f53e05727c73dc4e7eae86b5aa802dad9e
AndresL230 <andreslopez.23061@gmail.com>
2025-09-09 00:02:57 -0400

    HW 1

--- assigns/01/MySolution/Payroll.java
+++ assigns/01/MySolution/Payroll.java
@@ -3,27 +3,79 @@ public class Payroll {
     public static final int INITIAL_MAXIMUM_SIZE = 1024;
 
     public Payroll() {
-	/* your code */
+        current_size = 0;
+        maximum_size = INITIAL_MAXIMUM_SIZE;
+        people = new Employee[maximum_size];
+        
+    }
+
+    public int size() {
+        return current_size;
+    }
+
+    public void print(){
+        for (Employee e: people){
+            System.out.println("ID: "+e.ID+", Name: "+e.name+", Salary: "+e.salary);
+        }
     }
     
     public void add_employee(Employee newbie) {
-	/* your code */
+        if (current_size < maximum_size){
+            people[current_size+1] = newbie;
+            current_size+=1;
+        }
+        else{
+            Employee[] p = new Employee[maximum_size*2]
+            for (int i = 0; i < current_size; i++){
+                p[i] = people[i];
+            }
+            people = p;
+            people[current_size+1] = newbie;
+            current_size+=1;
+        }
     }
 
     public void remove_employee(int i) throws EmployeeIndexException {
-	/* your code */
+        if(i >= current_size || i == 0){
+            throw new EmployeeIndexException();
+        }
+        else{
+            for(int j = i; j < current_size; j++){
+                people[j] = people[j+1];
+            }
+            current_size-=1;
+        }
     }
     
     public int find_employee(String name) throws EmployeeNotFoundException {
-	/* your code */
+        for(int i = 0; i < current_size; i++){
+            if(name.equals(people[i].name))
+                return i;
+        }
+        throw new EmployeeNotFoundException();
     }
 
     public void add_payroll(Payroll source) {
-	/* your code */
+        int size = current_size + source.current_size;
+
+        if(size >= maximum_size){
+            Employee[] p = new Employee[maximum_size*2];
+            for(int i = 0; i<current_size; i++)
+                p[i] = people[i];
+            for(int i = 0; i<source.current_size; i++)
+                p[i+current_size] = source.people[i];
+            people = p;
+        }
+        else{
+            for(int i = current_size; i < source.current_size; i++)
+                people[i] = source.people[i];
+            
+        }
+        current_size = size;
     }
 
     public void copy_payroll(Payroll source) {
-	/* your code */
+        add_payroll(source);
     }
 
     private Employee people[];
