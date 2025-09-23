81ffe3c6b1f0ff88d1ab9ead0d9d2c56e5a1b7e3
AndresL230 <andreslopez.23061@gmail.com>
2025-09-23 15:12:30 -0400

    sync

---
+++ libraries/MyLibrary/BinarySearch.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import edu.princeton.cs.algs4.*;
+
+public class BinarySearch {
+    public BinarySearch(){
+    }
+
+    public int indexOf(int[] a, int key){
+        int lo = 0;
+        int hi = a.length - 1;
+        int mid;
+
+        while(lo <= hi){
+            mid = lo + (hi - lo)/2;
+
+            if (key < a[mid])
+                hi = mid-1;
+            else if (key > a[mid])
+                lo = mid + 1;
+            else 
+                return mid;
+        }
+        return -1;
+    }
+}
